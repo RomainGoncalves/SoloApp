b03b91f69f342ca8a3b264298d1cf88c4347c41c
Romain Goncalves <goncalves.romain@gmail.com>
2013-09-11 09:30:02 +1000

    Config Database

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'soloapp',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'aries86',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
