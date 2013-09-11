8bb17471b742ffeeeaa7ec55bb961438bfffe4b6
Romain Goncalves <goncalves.romain@gmail.com>
2013-09-11 22:04:22 +1000

    Created login form

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -4,5 +4,28 @@
 
 <h2>Login</h2>
 
+{{ Form::open(array('class' => 'form-horizontal', 'role' => 'form')) }}
+	<div class="form-group">
+	{{ Form::label('username', 'Username', array('class' => 'control-label col-lg-1', 'for' => 'username')) }}
+	<div class="col-lg-6">
+	{{ Form::text('username', '', array('class' => 'form-control col-lg-6', 'id' => 'username', 'placeholder' => 'Username')) }}
+	</div>
+  </div>
+
+  <div class="form-group">
+
+	{{ Form::label('password', 'Password', array('class' => 'control-label col-lg-1', 'for' => 'username')) }}
+	<div class="col-lg-6">
+	{{ Form::password('password', array('class' => 'form-control col-lg-6', 'id' => 'password', 'placeholder' => 'Password')) }}
+	</div>
+  </div>
+  <div class="form-group">
+    <div class="col-lg-offset-6">
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </div>
+  </div>
+
+{{ Form::close() }}
+
 
 @endsection
